ef37c14ef4a7186e19e18e2bfd7f9fd30975dc75
Travis CI <ghoul.ahmed5@gmail.com>
2019-12-16 22:17:49 +0100

    check login + add role check request

--- src/main/java/org/ghoul/SecurityConfig.java
+++ src/main/java/org/ghoul/SecurityConfig.java
@@ -3,35 +3,45 @@ package org.ghoul;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	public void globalConfig(AuthenticationManagerBuilder auth) throws Exception {
  
-	    auth.inMemoryAuthentication().withUser("admin").password("123").roles("ADMIN","PROF");
-        auth.inMemoryAuthentication().withUser("prof1").password("123").roles("PROF");
-        auth.inMemoryAuthentication().withUser("et1").password("123").roles("ETUDIANT");
-        auth.inMemoryAuthentication().withUser("sco1").password("123").roles("SCOLARITE");
+	    auth.inMemoryAuthentication().withUser("admin").password("{noop}123").roles("ADMIN","PROF");
+        auth.inMemoryAuthentication().withUser("prof1").password("{noop}123").roles("PROF");
+        auth.inMemoryAuthentication().withUser("et1").password("{noop}123").roles("ETUDIANT");
+        auth.inMemoryAuthentication().withUser("sco1").password("{noop}123").roles("SCOLARITE");
 	
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http
-		.authorizeRequests()
-		.anyRequest()
-		.authenticated()
-		.and()
-		.formLogin()
-        .loginPage("/login")
-        .permitAll();
+		  http
+          .csrf().disable()
+          .authorizeRequests()
+          .antMatchers("/css/**","/js/**","/images/**").permitAll()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .formLogin()
+          .loginPage("/login")
+          .permitAll()
+          .defaultSuccessUrl("/index.html")
+          .and()
+          .logout()
+          .invalidateHttpSession(true)
+          .logoutUrl("/logout")
+          .permitAll();
 	}
 	
 	
--- src/main/java/org/ghoul/WebMVCConfig.java
+++ src/main/java/org/ghoul/WebMVCConfig.java
@@ -3,11 +3,11 @@ package org.ghoul;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+ 
+ 
 @Configuration
 public class WebMVCConfig extends WebMvcConfigurerAdapter{
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
+    public void addViewControsllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/logout").setViewName("login");
     }
